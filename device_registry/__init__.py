b0fb4663db63dfb1f7ac615890801163eedad477
Hassan <hassan.mallah@cleverbots.ru>
2021-08-22 12:28:46 +0300

    shelve + DB

--- device_registry/__init__.py
+++ device_registry/__init__.py
@@ -1,15 +1,29 @@
-from flask import Flask
+from flask import Flask, g
 import os
 import markdown
+import shelve
 
 # Create Flask App - instance of Flask
 app = Flask(__name__)
 
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = shelve.open("devices.db")
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
 @app.route("/")
 def index():
-    """ Present some documentation """
-
     # Open README.md File
     with open(os.path.dirname(app.root_path) + '/README.md') as file:
+        # read the content of the file
         content = file.read()
+        # convert to HTML
         return markdown.markdown(content)
